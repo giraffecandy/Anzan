070975648134893c39715c7327da0b6bf81c5640
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-13 19:06:41 +0900

    起動すると✕のレイアウトが出てくる

--- app/src/main/java/app/babachan/anzan/MainActivity.kt
+++ app/src/main/java/app/babachan/anzan/MainActivity.kt
@@ -1,11 +1,54 @@
 package app.babachan.anzan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val number = (0..999).random()
+        val number1 = (0..99).random()
+
+        val operatorList = listOf<String>("+", "-")
+        val index = (0..1).random()
+        val operator = operatorList[index]
+
+        operatorTextView.text = operator
+        intent.putExtra("OPERATOR", operator)
+
+        textView.text = number.toString()
+        textView2.text = number1.toString()
+
+        intent.putExtra("NUMBER", number)
+        intent.putExtra("NUMBER1", number1)
+
+       val enterAnswer = answerTextView.editableText.toString()
+        val answer = findViewById(R.id.answerTextView) as EditText
+        val intEnterAnswer = enterAnswer.toIntOrNull()
+//        val answer = findViewById<EditText>(R.id.answerTextView)
+
+
+        var cpuAnswer = number + number1
+        intent.putExtra("ENTER",enterAnswer)
+
+
+
+            if (intEnterAnswer == cpuAnswer) {
+                val intent: Intent = Intent(this, RightAnswer::class.java)
+                startActivity(intent)
+            } else {
+                val intent: Intent = Intent(this, WrongAnswer::class.java)
+                startActivity(intent)
+            }
+
+
+
     }
 }
